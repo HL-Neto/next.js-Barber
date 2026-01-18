9f9320451b94784f9390ee0441f522d82bc1d6df
HL-Neto <humbertolunaneto@gmail.com>
2026-01-17 23:38:27 -0300

    vers 5

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,8 @@
 import styles from "./hero.module.css"
 import Image from "next/image";
 import Logo from "@/app/assets/Logo do Ferreira's Barber Studio.png";
+import { Scissors, Clock, MapPin } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 
 
@@ -8,9 +10,26 @@ export default function Hero(){
     return(
         <div  className={styles.hero}> 
 
-          <div className={styles.center_logo}>
+            <div className={styles.center_logo}>
                 <Image src={Logo} alt="Ferreira's Barber Studio" className={styles.logo} priority />
             </div>
+            
+            <div className="pt-48">
+
+            <span > Experiência premium em barbearia masculina. Tradição, qualidade e estilo em </span><br/>
+            <span className=" pl-56">cada detalhe.</span>
+
+            <div className="pt-20 pl-24 flex gap-4">
+                <Button  size="lg">Agendar Horário</Button><Button  size="lg">Nossos Serviços</Button>
+            </div>
+            
+            <div className="flex items-center pt-14 pl-44">
+              <Clock className="w-4 h-4 text-primary" />
+              <span>Seg - Sáb: 09:00 - 20:00</span>
+            </div>
+            </div>
+            
+            
 
         </div>
 
--- app/components/section/agendamento.tsx
+++ app/components/section/agendamento.tsx
@@ -6,20 +6,24 @@ export default function agendamento(){
         <div  className={styles.agendamento}> 
 
 
-                
-            <h1 className="text-primary text-sm uppercase tracking-widest font-body"> 
+        <div className="container mx-auto px-6 relative z-10">
+            <h1 className="text-primary text-sm uppercase tracking-widest font-body  pl-[36%]"> 
                 Pronto para uma transformação? 
             </h1>
             
-            <h2 className="font-display text-4xl md:text-6xl font-bold mt-4 mb-6">
+            <h2 className="font-display text-4xl md:text-6xl font-bold mt-4 mb-6 pl-56">
                 Agende seu Horário
             </h2>
             
-            <p className="text-muted-foreground font-body text-lg max-w-2xl mx-auto mb-12">
-                Reserve agora e garanta uma experiência premium. Atendemos com hora marcada
-                para sua comodidade.
-            </p> 
-
+            <div className="text-muted-foreground font-body text-lg max-w-2xl mx-auto mb-20 ">
+                <p >
+                    Reserve agora e garanta uma experiência premium. A  tendemos com hora marcada
+                
+                </p>
+                <p className="pl-[40%]">
+                    para sua comodidade.</p> 
+                </div>
+        </div>
 
 
 
